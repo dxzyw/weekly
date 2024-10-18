d00c0281c58882b198e592ce8b1b60557fbacb85
unknown <17621764533@163.com>
2024-10-18 11:46:27 +0800

    1018-lastest

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export const SITE = {
   title: '开源周刊',
   author: 'dxzyw',
-  description: '记录工程师的不枯燥生活，每周一发布，欢迎关注',
+  description: '记录沪漂工程师的不枯燥生活，每周六发布，欢迎关注',
   keywords: '运维,Weekly,Blog,devops,macOS,SRE，k8s',
   icon: 'https://img.picui.cn/free/2024/10/15/670e07de8c1a1.png',
   pic: 'https://img.picui.cn/free/2024/10/15/670e07de8c1a1.png',
