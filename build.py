832620bff85835c0722d45bd96c0a5f2ad9db582
大侠之运维 <141013297+dxzyw@users.noreply.github.com>
2024-10-24 15:38:47 +0800

    Update build.py

--- build.py
+++ build.py
@@ -4,14 +4,14 @@ import re
 import urllib.parse
 
 def fetch_ci_time(file_path):
-    url = f"https://api.github.com/repos/tw93/weekly/commits?path={file_path}&page=1&per_page=1"
+    url = f"https://api.github.com/repos/dxzyw/weekly/commits?path={file_path}&page=1&per_page=1"
     response = httpx.get(url)
     ci_time = response.json()[0]["commit"]["committer"]["date"].split("T")[0]
     return ci_time
 
 if __name__ == "__main__":
     with open('README.md', 'w') as readme_file, open('RECENT.md', 'w') as recent_file:
-        readme_file.write("# 开源周刊\n\n> 记录工程师 Tw93 的不枯燥生活，欢迎订阅，也欢迎 [推荐](https://github.com/tw93/weekly/discussions/22) 你的好东西，Fork 自用可见 [开发文档](https://github.com/tw93/weekly/blob/main/Deploy.md)，期待你玩得开心~\n\n")
+        readme_file.write("# 开源周刊\n\n> 记录工程师 Tw93 的不枯燥生活，欢迎订阅，也欢迎 [推荐](https://github.com/dxzyw/weekly/discussions/22) 你的好东西，Fork 自用可见 [开发文档](https://github.com/tw93/weekly/blob/main/Deploy.md)，期待你玩得开心~\n\n")
 
         for root, dirs, filenames in os.walk('./src/pages/posts'):
             filenames = sorted(filenames, key=lambda x: float(re.findall(r"(\d+)", x)[0]), reverse=True)
